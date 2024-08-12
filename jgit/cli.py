7552041f584914f0889eac71557bf5ff0480fff5
jeanjohnsen <>
2024-08-12 21:18:03 +0200

    uhmm fixed my hashing command

--- jgit/cli.py
+++ jgit/cli.py
@@ -16,18 +16,18 @@ def parse_args():
     commands.required = True
     
     init_parser = commands.add_parser('init')
-    init_parser.set_defaults(function=init)
+    init_parser.set_defaults(func=init)
     
-    hash_object_parser = commands.add_parser('hash_parser')
-    hash_object_parser.set_defaults(function=hash_object)
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
     hash_object_parser.add_argument('file')
     
     cat_file_parser = commands.add_parser('cat_file')
-    cat_file_parser.set_defaults(function=cat_file)
+    cat_file_parser.set_defaults(func=cat_file)
     cat_file_parser.add_argument('object')
     
     write_tree_parser = commands.add_parser('write_tree')
-    write_tree_parser.set_defaults(function=write_tree)
+    write_tree_parser.set_defaults(func=write_tree)
     
     return parser.parse_args()
     
