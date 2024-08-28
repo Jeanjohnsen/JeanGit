717803f5b400aad81344976265392a2142455fc2
jeanjohnsen <geniusnh4@gmail.com>
2024-08-28 21:00:11 +0200

    create tag refs

--- jgit/base.py
+++ jgit/base.py
@@ -7,118 +7,117 @@ from collections import namedtuple
 from . import data
 
 
-def write_tree (directory='.'):
+def write_tree(directory='.'):
     entries = []
-    with os.scandir (directory) as it:
+    with os.scandir(directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
-            if is_ignored (full):
+            if is_ignored(full):
                 continue
 
-            if entry.is_file (follow_symlinks=False):
+            if entry.is_file(follow_symlinks=False):
                 type_ = 'blob'
-                with open (full, 'rb') as f:
-                    oid = data.hash_object (f.read ())
-            elif entry.is_dir (follow_symlinks=False):
+                with open(full, 'rb') as f:
+                    oid = data.hash_object(f.read())
+            elif entry.is_dir(follow_symlinks=False):
                 type_ = 'tree'
-                oid = write_tree (full)
-            entries.append ((entry.name, oid, type_))
+                oid = write_tree(full)
+            entries.append((entry.name, oid, type_))
 
-    tree = ''.join (f'{type_} {oid} {name}\n'
-                    for name, oid, type_
-                    in sorted (entries))
-    return data.hash_object (tree.encode (), 'tree')
+    tree = ''.join(f'{type_} {oid} {name}\n'
+                   for name, oid, type_
+                   in sorted(entries))
+    return data.hash_object(tree.encode(), 'tree')
 
 
-def _iter_tree_entries (oid):
+def _iter_tree_entries(oid):
     if not oid:
         return
-    tree = data.get_object (oid, 'tree')
-    for entry in tree.decode ().splitlines ():
-        type_, oid, name = entry.split (' ', 2)
+    tree = data.get_object(oid, 'tree')
+    for entry in tree.decode().splitlines():
+        type_, oid, name = entry.split(' ', 2)
         yield type_, oid, name
 
 
-def get_tree (oid, base_path=''):
+def get_tree(oid, base_path=''):
     result = {}
-    for type_, oid, name in _iter_tree_entries (oid):
+    for type_, oid, name in _iter_tree_entries(oid):
         assert '/' not in name
         assert name not in ('..', '.')
         path = base_path + name
         if type_ == 'blob':
             result[path] = oid
         elif type_ == 'tree':
-            result.update (get_tree (oid, f'{path}/'))
+            result.update(get_tree(oid, f'{path}/'))
         else:
             assert False, f'Unknown tree entry {type_}'
     return result
 
 
-def _empty_current_directory ():
-    for root, dirnames, filenames in os.walk ('.', topdown=False):
+def _empty_current_directory():
+    for root, dirnames, filenames in os.walk('.', topdown=False):
         for filename in filenames:
-            path = os.path.relpath (f'{root}/{filename}')
-            if is_ignored (path) or not os.path.isfile (path):
+            path = os.path.relpath(f'{root}/{filename}')
+            if is_ignored(path) or not os.path.isfile(path):
                 continue
-            os.remove (path)
+            os.remove(path)
         for dirname in dirnames:
-            path = os.path.relpath (f'{root}/{dirname}')
-            if is_ignored (path):
+            path = os.path.relpath(f'{root}/{dirname}')
+            if is_ignored(path):
                 continue
             try:
-                os.rmdir (path)
+                os.rmdir(path)
             except (FileNotFoundError, OSError):
                 # Deletion might fail if the directory contains ignored files,
                 # so it's OK
                 pass
 
 
-def read_tree (tree_oid):
-    _empty_current_directory ()
-    for path, oid in get_tree (tree_oid, base_path='./').items ():
-        os.makedirs (os.path.dirname (path), exist_ok=True)
-        with open (path, 'wb') as f:
-            f.write (data.get_object (oid))
+def read_tree(tree_oid):
+    _empty_current_directory()
+    for path, oid in get_tree(tree_oid, base_path='./').items():
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, 'wb') as f:
+            f.write(data.get_object(oid))
 
 
-def commit (message):
-    commit = f'tree {write_tree ()}\n'
+def commit(message):
+    commit = f'tree {write_tree()}\n'
 
-    HEAD = data.get_ref ('HEAD')
+    HEAD = data.get_ref('HEAD')
     if HEAD:
         commit += f'parent {HEAD}\n'
 
     commit += '\n'
     commit += f'{message}\n'
 
-    oid = data.hash_object (commit.encode (), 'commit')
+    oid = data.hash_object(commit.encode(), 'commit')
 
-    data.update_ref ('HEAD', oid)
+    data.update_ref('HEAD', oid)
 
     return oid
 
 
-def checkout (oid):
-    commit = get_commit (oid)
-    read_tree (commit.tree)
-    data.update_ref ('HEAD', oid)
+def checkout(oid):
+    commit = get_commit(oid)
+    read_tree(commit.tree)
+    data.update_ref('HEAD', oid)
 
 
-def create_tag (name, oid):
-    # TODO Actually create the tag
-    pass
+def create_tag(name, oid):
+    data.get_ref(f'refs/tags/{name}', oid)
 
 
-Commit = namedtuple ('Commit', ['tree', 'parent', 'message'])
+Commit = namedtuple('Commit', ['tree', 'parent', 'message'])
 
 
-def get_commit (oid):
+def get_commit(oid):
     parent = None
 
-    commit = data.get_object (oid, 'commit').decode ()
-    lines = iter (commit.splitlines ())
-    for line in itertools.takewhile (operator.truth, lines):
-        key, value = line.split (' ', 1)
+    commit = data.get_object(oid, 'commit').decode()
+    lines = iter(commit.splitlines())
+    for line in itertools.takewhile(operator.truth, lines):
+        key, value = line.split(' ', 1)
         if key == 'tree':
             tree = value
         elif key == 'parent':
@@ -126,9 +125,9 @@ def get_commit (oid):
         else:
             assert False, f'Unknown field {key}'
 
-    message = '\n'.join (lines)
-    return Commit (tree=tree, parent=parent, message=message)
+    message = '\n'.join(lines)
+    return Commit(tree=tree, parent=parent, message=message)
 
 
-def is_ignored (path):
-    return '.jgit' in path.split ('/')
+def is_ignored(path):
+    return '.jgit' in path.split('/')
--- jgit/cli.py
+++ jgit/cli.py
@@ -7,98 +7,98 @@ from . import base
 from . import data
 
 
-def main ():
-    args = parse_args ()
-    args.func (args)
+def main():
+    args = parse_args()
+    args.func(args)
 
 
-def parse_args ():
-    parser = argparse.ArgumentParser ()
+def parse_args():
+    parser = argparse.ArgumentParser()
 
-    commands = parser.add_subparsers (dest='command')
+    commands = parser.add_subparsers(dest='command')
     commands.required = True
 
-    init_parser = commands.add_parser ('init')
-    init_parser.set_defaults (func=init)
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
 
-    hash_object_parser = commands.add_parser ('hash-object')
-    hash_object_parser.set_defaults (func=hash_object)
-    hash_object_parser.add_argument ('file')
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
 
-    cat_file_parser = commands.add_parser ('cat-file')
-    cat_file_parser.set_defaults (func=cat_file)
-    cat_file_parser.add_argument ('object')
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object')
 
-    write_tree_parser = commands.add_parser ('write-tree')
-    write_tree_parser.set_defaults (func=write_tree)
+    write_tree_parser = commands.add_parser('write-tree')
+    write_tree_parser.set_defaults(func=write_tree)
 
-    read_tree_parser = commands.add_parser ('read-tree')
-    read_tree_parser.set_defaults (func=read_tree)
-    read_tree_parser.add_argument ('tree')
+    read_tree_parser = commands.add_parser('read-tree')
+    read_tree_parser.set_defaults(func=read_tree)
+    read_tree_parser.add_argument('tree')
 
-    commit_parser = commands.add_parser ('commit')
-    commit_parser.set_defaults (func=commit)
-    commit_parser.add_argument ('-m', '--message', required=True)
+    commit_parser = commands.add_parser('commit')
+    commit_parser.set_defaults(func=commit)
+    commit_parser.add_argument('-m', '--message', required=True)
 
-    log_parser = commands.add_parser ('log')
-    log_parser.set_defaults (func=log)
-    log_parser.add_argument ('oid', nargs='?')
+    log_parser = commands.add_parser('log')
+    log_parser.set_defaults(func=log)
+    log_parser.add_argument('oid', nargs='?')
 
-    checkout_parser = commands.add_parser ('checkout')
-    checkout_parser.set_defaults (func=checkout)
-    checkout_parser.add_argument ('oid')
+    checkout_parser = commands.add_parser('checkout')
+    checkout_parser.set_defaults(func=checkout)
+    checkout_parser.add_argument('oid')
 
-    tag_parser = commands.add_parser ('tag')
-    tag_parser.set_defaults (func=tag)
-    tag_parser.add_argument ('name')
-    tag_parser.add_argument ('oid', nargs='?')
+    tag_parser = commands.add_parser('tag')
+    tag_parser.set_defaults(func=tag)
+    tag_parser.add_argument('name')
+    tag_parser.add_argument('oid', nargs='?')
 
-    return parser.parse_args ()
-
-
-def init (args):
-    data.init ()
-    print (f'Initialized empty jgit repository in {os.getcwd()}/{data.GIT_DIR}')
+    return parser.parse_args()
 
 
-def hash_object (args):
-    with open (args.file, 'rb') as f:
-        print (data.hash_object (f.read ()))
+def init(args):
+    data.init()
+    print(f'Initialized empty jgit repository in {os.getcwd()}/{data.GIT_DIR}')
 
 
-def cat_file (args):
-    sys.stdout.flush ()
-    sys.stdout.buffer.write (data.get_object (args.object, expected=None))
+def hash_object(args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
 
 
-def write_tree (args):
-    print (base.write_tree ())
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object, expected=None))
 
 
-def read_tree (args):
-    base.read_tree (args.tree)
+def write_tree(args):
+    print(base.write_tree())
 
 
-def commit (args):
-    print (base.commit (args.message))
+def read_tree(args):
+    base.read_tree(args.tree)
 
 
-def log (args):
-    oid = args.oid or data.get_ref ('HEAD')
+def commit(args):
+    print(base.commit(args.message))
+
+
+def log(args):
+    oid = args.oid or data.get_ref('HEAD')
     while oid:
-        commit = base.get_commit (oid)
+        commit = base.get_commit(oid)
 
-        print (f'commit {oid}\n')
-        print (textwrap.indent (commit.message, '    '))
-        print ('')
+        print(f'commit {oid}\n')
+        print(textwrap.indent(commit.message, '    '))
+        print('')
 
         oid = commit.parent
 
 
-def checkout (args):
-    base.checkout (args.oid)
+def checkout(args):
+    base.checkout(args.oid)
 
 
-def tag (args):
-    oid = args.oid or data.get_ref ('HEAD')
-    base.create_tag (args.name, oid)
+def tag(args):
+    oid = args.oid or data.get_ref('HEAD')
+    base.create_tag(args.name, oid)
